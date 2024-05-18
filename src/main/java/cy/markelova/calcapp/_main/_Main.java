901bff3778d6d973b228ff46df33177b75807d36
Iuliia M <89371934+JuMarkelova@users.noreply.github.com>
2024-05-18 21:17:16 +0300

    using one variable instead of three, removing extra indentation, using English

--- src/main/java/cy/markelova/calcapp/_main/_Main.java
+++ src/main/java/cy/markelova/calcapp/_main/_Main.java
@@ -6,17 +6,13 @@ import static cy.markelova.calcapp.util.Calculator.*;
 public class _Main {
 
     public static void main(String[] args) {
-
         int num1 = 28;
         int num2 = 689;
-
-        int resultSum = addNumbers(num1, num2);
-        printResult(num1, num2, resultSum, "Сумма");
-
-        int resultSubtract = subtractNumbers(num1, num2);
-        printResult(num1, num2, resultSubtract, "Разность");
-
-        int resultProduct = multiplyNumbers(num1, num2);
-        printResult(num1, num2, resultProduct, "Прозведение");
+        int result = addNumbers(num1, num2);
+        printResult(num1, num2, result, "Addition");
+        result = subtractNumbers(num1, num2);
+        printResult(num1, num2, result, "Subtraction");
+        result = multiplyNumbers(num1, num2);
+        printResult(num1, num2, result, "Multiplication");
     }
 }
